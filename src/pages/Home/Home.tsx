f7b48fa90608cbb2125358cd795afea7db18e00f
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-08-03 12:22:12 -0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,6 @@
-import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router";
 
 export default function Home() {
-    const navigate = useNavigate();
     return (<>
         <h1>Home 😀</h1>
-        <Button variant="primary" onClick={() => navigate('/elementos')}>Ir a la Elementos</Button>
     </>);
 }
