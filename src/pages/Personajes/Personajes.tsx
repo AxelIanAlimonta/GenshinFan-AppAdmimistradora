a94d341d82c99225b804ef62724f5fc5a1cddb3f
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-08-03 12:20:19 -0300

    feat(personajes): agregar búsqueda y orden alfabético en la lista
    Se añadió un campo de búsqueda para filtrar personajes por nombre y se ordenó la lista alfabéticamente. Además, se actualizó el diseño con un contenedor flex para el botón de agregar y la barra de búsqueda.

--- src/pages/Personajes/Personajes.tsx
+++ src/pages/Personajes/Personajes.tsx
@@ -1,29 +1,56 @@
 import './Personajes.css';
 import type { Personaje } from '../../types/Personaje';
 import usePersonajes from '../../hooks/Personajes/usePersonajes';
-import { Button, Col, Row } from 'react-bootstrap';
+import { Button, Col, FloatingLabel, FormControl, Row } from 'react-bootstrap';
 import MostrarImagen from '../../components/MostrarImagen';
 import Loading from '../../components/Loading';
 import { useNavigate } from 'react-router';
+import { useState } from 'react';
 
 
 export default function Personajes() {
 
     const { personajes, loading, deletePersonaje } = usePersonajes();
     const navigate = useNavigate();
+    const [busqueda, setBusqueda] = useState('');
 
     if (loading) {
         return <Loading />;
     }
 
+
+    // Ordenar personajes por nombre, los nombres podrían ser undefined
+    const personajesOrdenados = [...personajes].sort((a: Personaje, b: Personaje) => {
+        const nombreA = a.nombre?.toLowerCase() || '';
+        const nombreB = b.nombre?.toLowerCase() || '';
+        return nombreA.localeCompare(nombreB);
+    });
+
+    const personajesFiltrados = personajesOrdenados.filter((personaje: Personaje) =>
+        personaje.nombre?.toLowerCase().includes(busqueda.toLowerCase())
+    );
+
+
     return (<>
         <main>
-            <Button variant="success" className="mb-3" onClick={() => navigate('/personajes/crear')}>
-                Agregar Personaje
-            </Button>
+
+            <div className="personajes-agregarYbuscador-contenedor">
+                <Button variant="success" className="mb-3" onClick={() => navigate('/personajes/crear')}>
+                    Agregar Personaje
+                </Button>
+                <FloatingLabel className="mb-3" controlId="floatingInput" label="Buscar personaje">
+                    <FormControl
+                        type="text"
+                        placeholder="Buscar personaje"
+                        className="mb-3"
+                        value={busqueda}
+                        onChange={(e) => setBusqueda(e.target.value)}
+                    />
+                </FloatingLabel>
+            </div>
 
             <div className="contenedorDePersonajes">
-                {personajes.map((personaje: Personaje) => (
+                {personajesFiltrados.map((personaje: Personaje) => (
                     <div key={personaje.id} className='personaje-item'>
                         <MostrarImagen src={personaje.avatarURL} alt={personaje.nombre} style={{ width: 75 }} />
                         <h3 className='personaje-nombre'>{personaje.nombre}</h3>
